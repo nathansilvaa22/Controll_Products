d494b9f2c8975fca0ff8a31cee9f333e130fe926
Nathan Silva <claricelucasmoreira8@gmail.com>
2024-07-11 22:57:57 -0300

    instanciando a classe

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,5 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        
+
+
+        sc.close();
     }
 }
